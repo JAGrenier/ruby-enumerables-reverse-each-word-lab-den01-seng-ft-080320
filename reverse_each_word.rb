86fdcc4fe04b3db65bfa128a43c84a82926b8f86
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-25 20:20:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
-def reverse_each_word (string)
-   string=string.split(" ")
-   new_arr = string.collect do |a|
-     a.reverse << new_arr
+def reverse_each_word (sentence)
+  sentence.split.collect {|word| word.reverse}.join('')
 end
 end
